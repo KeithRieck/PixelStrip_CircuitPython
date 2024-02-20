17edb921510e9352b3a191dd9deacf059d427e29
Keith Rieck <KeithRieck@gmail.com>
2024-02-19 21:23:23 -0600

    Rework I2C examples

---
+++ examples/i2c_examples/i2c_animation_tester_roborio/LightsSubsystem.java
@@ -0,0 +1,85 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LightsSubsystem extends SubsystemBase {
+
+  public static int I2C_ADDRESS = 0x41;
+  public static final int MAX_ANIMATIONS = 3;
+  public static final int MAX_STRIPS = 4;
+
+  private byte[] currentAnimation = new byte[MAX_STRIPS];
+  private byte[] nextAnimation = new byte[MAX_STRIPS];
+  private byte[] dataOut = new byte[1];
+
+  private I2C i2c = null;
+
+  /**
+   * Subsystem to controll all the lights running on the external lights
+   * microcontroller.
+   * <p>
+   * The whole "lights plan" should be implemented from within this subsystem,
+   * controlled mostly from within the {@code periodic} method.
+   * <p>
+   * If you need external subsystems and commands to communicate into this class,
+   * create "modes" within the class to indicate external states. For instance,
+   * if you need a command to indicate when the robot is preparing to shoot, add
+   * a {@code setShootingMode(boolean)} to this class.
+   */
+  public LightsSubsystem() {
+    i2c = new I2C(Port.kOnboard, I2C_ADDRESS);
+    clearAllAnimations();
+  }
+
+  @Override
+  public void periodic() {
+    // TODO: monitor internal robot state and change animations as necessary
+    sendAllAnimations();
+  }
+
+  /**
+   * Clear out all the strips and stop all animation.
+   * <br>
+   * This should not be called from outside this subsystem.
+   */
+  protected void clearAllAnimations() {
+    for (int s = 0; s < MAX_STRIPS; s++) {
+      Integer b = Integer.valueOf(((s << 4) & 0xF0) | (MAX_ANIMATIONS & 0x0F));
+      nextAnimation[s] = b.byteValue();
+      currentAnimation[s] = (byte) 0;
+    }
+  }
+
+  /**
+   * Set one strip to have the numbered animation.
+   * <br>
+   * This should not be called from outside this subsystem.
+   */
+  protected void setAnimation(int stripNumber, int animNumber) {
+    Integer b = Integer.valueOf(((stripNumber << 4) & 0xF0) | (animNumber & 0x0F));
+    nextAnimation[stripNumber] = b.byteValue();
+  }
+
+  /**
+   * Push out all animation changes to the Pico. <br/>
+   * This program takes a <em>lazy</em> approach, in that animation signals are
+   * only sent out if they <em>need</em> to change. Signals are only sent if the
+   * desired animation is different from the current animation.
+   * This prevents redundant, unnecessary changes from dominating the I2C bus.
+   */
+  private void sendAllAnimations() {
+    for (int s = 0; s < MAX_STRIPS; s++) {
+      if (nextAnimation[s] != currentAnimation[s]) {
+        sendOneAnimation(s);
+        currentAnimation[s] = nextAnimation[s];
+      }
+    }
+  }
+
+  private void sendOneAnimation(int stripNumber) {
+    dataOut[0] = nextAnimation[stripNumber];
+    i2c.writeBulk(dataOut, dataOut.length);
+  }
+}
--- examples/i2c_examples/i2c_animation_tester_roborio/Robot.java
+++ examples/i2c_examples/i2c_animation_tester_roborio/Robot.java
@@ -2,8 +2,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.I2C.Port;
 
 /**
  * This robot program tests that animations on a Raspberry Pi Pico can be
@@ -11,38 +9,36 @@ import edu.wpi.first.wpilibj.I2C.Port;
  */
 public class Robot extends TimedRobot {
 
-  public static int I2C_ADDRESS = 0x41;
-  public static final int MAX_ANIMATIONS = 3;
-  public static final int MAX_STRIPS = 4;
+  LightsSubsystem m_LightsSubsystem;
+  XboxController xbox;
 
-  private byte[] currentAnimation = new byte[MAX_STRIPS];
-  private byte[] nextAnimation = new byte[MAX_STRIPS];
   private int myStrip = 0;
   private int myAnim = 0;
-  private byte[] dataOut = new byte[1];
-
-  private XboxController xbox = null;
-  private I2C i2c = null;
 
   @Override
   public void robotInit() {
     xbox = new XboxController(0);
-    i2c = new I2C(Port.kOnboard, I2C_ADDRESS);
-    clearAllAnimations();
+    m_LightsSubsystem = new LightsSubsystem();
   }
 
-  /**
-   * Pressing the Left bumper button will change the animation on one of the strips.
-   * Pressing any of the A, B, X, or Y buttons selects which strip will be changed.
-   * Pressing the Right bumper button clears out all strip animations.
-   */
+  @Override
+  public void robotPeriodic() {
+    m_LightsSubsystem.periodic();
+  }
+
+  @Override
+  public void teleopInit() {
+    myStrip = 0;
+    myAnim = 0;
+  }
+
   @Override
   public void teleopPeriodic() {
     if (xbox.getLeftBumperPressed()) {
-      myAnim = (myAnim + 1) % MAX_ANIMATIONS;
-      setAnimation(myStrip, myAnim);
+      myAnim = (myAnim + 1) % m_LightsSubsystem.MAX_ANIMATIONS;
+      m_LightsSubsystem.setAnimation(myStrip, myAnim);
     } else if (xbox.getRightBumperPressed()) {
-      clearAllAnimations();
+      m_LightsSubsystem.clearAllAnimations();
     } else if (xbox.getYButtonPressed()) {
       myStrip = 0;
     } else if (xbox.getBButtonPressed()) {
@@ -52,48 +48,6 @@ public class Robot extends TimedRobot {
     } else if (xbox.getXButtonPressed()) {
       myStrip = 3;
     }
-    sendAllAnimations();
   }
 
-  /**
-   * Clear out all the strips and stop all animation.
-   */
-  private void clearAllAnimations() {
-    for (int s = 0; s < MAX_STRIPS; s++) {
-      Integer b = Integer.valueOf(((s << 4) & 0xF0) | (MAX_ANIMATIONS & 0x0F));
-      nextAnimation[s] = b.byteValue();
-      currentAnimation[s] = (byte) 0;
-    }
-    myStrip = 0;
-    myAnim = 0;
-  }
-
-  /**
-   * Set one strip to have the numbered animation.
-   */
-  private void setAnimation(int stripNumber, int animNumber) {
-    Integer b = Integer.valueOf(((stripNumber << 4) & 0xF0) | (animNumber & 0x0F));
-    nextAnimation[stripNumber] = b.byteValue();
-  }
-
-  /**
-   * Push out all animation changes to the Pico. <br/>
-   * This program takes a <em>lazy</em> approach, in that animation signals are
-   * only sent out if they <em>need</em> to change. Signals are only sent if the
-   * desired animation is different from the current animation.
-   * This prevents redundant, unnecessary changes from dominating the I2C bus.
-   */
-  private void sendAllAnimations() {
-    for (int s = 0; s < MAX_STRIPS; s++) {
-      if (nextAnimation[s] != currentAnimation[s]) {
-        sendOneAnimation(s);
-        currentAnimation[s] = nextAnimation[s];
-      }
-    }
-  }
-
-  private void sendOneAnimation(int stripNumber) {
-    dataOut[0] = nextAnimation[stripNumber];
-    i2c.writeBulk(dataOut, dataOut.length);
-  }
 }
